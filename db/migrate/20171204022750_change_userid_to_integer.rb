9f1cc9815fd3832dafa9aeacd8f74ee3ca06bdc5
Robert Hustead <husteadrobert@gmail.com>
2017-12-04 13:08:44 +0900

    heroku

--- db/migrate/20171204022750_change_userid_to_integer.rb
+++
@@ -1,7 +0,0 @@
-class ChangeUseridToInteger < ActiveRecord::Migration[5.1]
-  def change
-    change_column :categories, :user_id, :integer
-    change_column :items, :user_id, :integer
-    change_column :lists, :user_id, :integer
-  end
-end
